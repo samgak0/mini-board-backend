cfb3aab2399908711d286d9407cb8c7ee6cf6da2
samgak <22789236+samgakhyeong@users.noreply.github.com>
2024-09-19 16:43:38 +0900

    Changed Repository to Service

--- src/main/java/shop/samgak/mini_board/controllers/UserController.java
+++ src/main/java/shop/samgak/mini_board/controllers/UserController.java
@@ -3,24 +3,22 @@ package shop.samgak.mini_board.controllers;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import shop.samgak.mini_board.entities.User;
-import shop.samgak.mini_board.repositories.UserRepository;
+import shop.samgak.mini_board.services.UserService;
 
 @RestController
-@RequestMapping("/api/users")
 @RequiredArgsConstructor
 @Slf4j
 public class UserController {
 
-    final UserRepository repository;
+    final UserService userService;
 
-    @GetMapping
-    public List<User> getAllPosts() {
-        return repository.findAll();
+    @GetMapping("/api/users")
+    public List<User> getAllUser() {
+        return userService.getAll();
     }
 }
