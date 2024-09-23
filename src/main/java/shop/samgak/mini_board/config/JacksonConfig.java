80c49002d93e527dc463b08188df414c145d1b79
samgak <22789236+samgakhyeong@users.noreply.github.com>
2024-09-23 16:13:13 +0900

    Changed LocalDateTimeToEpochSecondsSerializer to InnerClass

--- src/main/java/shop/samgak/mini_board/config/JacksonConfig.java
+++ src/main/java/shop/samgak/mini_board/config/JacksonConfig.java
@@ -1,11 +1,16 @@
 package shop.samgak.mini_board.config;
 
+import java.io.IOException;
 import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 
 @Configuration
@@ -15,7 +20,20 @@ public class JacksonConfig {
     public ObjectMapper objectMapper() {
         ObjectMapper objectMapper = new ObjectMapper();
         SimpleModule module = new SimpleModule();
-        module.addSerializer(LocalDateTime.class, new LocalDateTimeToEpochSecondsSerializer());
+
+        module.addSerializer(LocalDateTime.class, new JsonSerializer<LocalDateTime>() {
+            @Override
+            public void serialize(LocalDateTime value, JsonGenerator gen, SerializerProvider serializers)
+                    throws IOException {
+                if (value != null) {
+                    long epochSeconds = value.toEpochSecond(ZoneOffset.UTC);
+                    gen.writeNumber(epochSeconds);
+                } else {
+                    gen.writeNull();
+                }
+            }
+        });
+
         objectMapper.registerModule(module);
         return objectMapper;
     }
