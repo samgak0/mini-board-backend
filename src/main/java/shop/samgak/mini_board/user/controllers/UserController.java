97930240805e80a886c13498133d24be061cb86a
samgak <22789236+samgakhyeong@users.noreply.github.com>
2024-09-23 19:09:07 +0900

    Added check username is available

--- src/main/java/shop/samgak/mini_board/user/controllers/UserController.java
+++ src/main/java/shop/samgak/mini_board/user/controllers/UserController.java
@@ -2,6 +2,7 @@ package shop.samgak.mini_board.user.controllers;
 
 import java.net.URI;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import shop.samgak.mini_board.user.services.UserService;
@@ -21,11 +23,40 @@ import shop.samgak.mini_board.utility.ApiResponse;
 @RequestMapping("/api/users/")
 public class UserController {
 
+    private static final String SESSION_CHECKED_USER = "checked_user_name";
+
     final UserService userService;
+
+    @PostMapping("check/username")
+    public ResponseEntity<ApiResponse> checkUsername(@RequestParam String username, HttpSession session) {
+        try {
+            if (username == null || username.isEmpty()) {
+                return ResponseEntity.badRequest().body(new ApiResponse("username is empty", ApiResponse.Code.FAILURE));
+            }
+
+            boolean isExistUserName = userService.existUsername(username);
+            if (!isExistUserName) {
+                session.setAttribute(SESSION_CHECKED_USER, username);
+                return ResponseEntity.ok().body(new ApiResponse("username is available", true));
+            } else {
+                return ResponseEntity.status(HttpStatus.CONFLICT)
+                        .body(new ApiResponse("username already used", ApiResponse.Code.USED));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.internalServerError().body(new ApiResponse(e.getMessage(), false));
+        }
+    }
 
     @PostMapping("register")
     public ResponseEntity<ApiResponse> register(@RequestParam String username, @RequestParam String email,
-            @RequestParam String password) {
+            @RequestParam String password, HttpSession session) {
+        Object checkedUsername = session.getAttribute(SESSION_CHECKED_USER);
+        if (checkedUsername == null || !(checkedUsername instanceof String)) {
+            return ResponseEntity.badRequest()
+                    .body(new ApiResponse("username check not performed", false));
+        }
+
         try {
             Long id = userService.save(username, email, password);
             URI location = URI.create("/api/users/" + id + "/info");
--- src/main/java/shop/samgak/mini_board/utility/ApiResponse.java
+++ src/main/java/shop/samgak/mini_board/utility/ApiResponse.java
@@ -6,6 +6,27 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public class ApiResponse {
+
+    public ApiResponse(String message, boolean success) {
+        this(message, success ? Code.SUCCESS : Code.FAILURE);
+    }
+
+    public enum Code {
+        SUCCESS("Success"),
+        FAILURE("Failure"),
+        USED("AlreadyUsed");
+
+        private final String value;
+
+        Code(String value) {
+            this.value = value;
+        }
+
+        public String getValue() {
+            return value;
+        }
+    }
+
     private final String message;
-    private final boolean success;
+    private final Code code;
 }
