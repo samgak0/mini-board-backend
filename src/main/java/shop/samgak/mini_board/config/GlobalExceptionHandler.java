bbc1884852cbfc6aee534ddda3760c0b3ce91d2f
samgak <22789236+samgakhyeong@users.noreply.github.com>
2024-09-24 18:39:17 +0900

    Fixed error handling messages to be more clear

--- src/main/java/shop/samgak/mini_board/config/GlobalExceptionHandler.java
+++ src/main/java/shop/samgak/mini_board/config/GlobalExceptionHandler.java
@@ -6,12 +6,14 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import shop.samgak.mini_board.exceptions.ResourceNotFoundException;
+import shop.samgak.mini_board.utility.ApiResponse;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<String> handleResourceNotFound(ResourceNotFoundException ex) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    public ResponseEntity<ApiResponse> handleResourceNotFoundException(ResourceNotFoundException ex) {
+        ApiResponse response = new ApiResponse(ex.getMessage(), false);
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
     }
 }
