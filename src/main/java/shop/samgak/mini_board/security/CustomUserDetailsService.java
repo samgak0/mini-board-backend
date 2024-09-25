9bb6c43707d721b5db7c98fa877613a9bfd4aa28
samgak <22789236+samgakhyeong@users.noreply.github.com>
2024-09-25 18:39:03 +0900

    Modified log in to be linked to DB

--- src/main/java/shop/samgak/mini_board/security/CustomUserDetailsService.java
+++ src/main/java/shop/samgak/mini_board/security/CustomUserDetailsService.java
@@ -1,41 +1,31 @@
 package shop.samgak.mini_board.security;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import shop.samgak.mini_board.user.entities.User;
+import shop.samgak.mini_board.user.repositories.UserRepository;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
 
-    private static final Map<String, String> users = new HashMap<>();
-
-    static {
-        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-        users.put("user", passwordEncoder.encode("password"));
-    }
+    private final UserRepository userRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        String password = users.get(username);
 
-        if (password == null) {
-            throw new UsernameNotFoundException("Cannot Found User : " + username);
-        }
-        // UserDetails 객체 생성
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Cannot Found User : " + username));
+
         return org.springframework.security.core.userdetails.User
                 .withUsername(username)
-                .password(password)
+                .password(user.getPassword())
                 .authorities("USER")
                 .accountExpired(false)
                 .accountLocked(false)
