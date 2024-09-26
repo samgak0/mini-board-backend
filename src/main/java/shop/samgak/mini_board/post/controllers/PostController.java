7c295de96d20d54ea6d02716c5a744ff64eb9173
samgak <22789236+samgakhyeong@users.noreply.github.com>
2024-09-26 19:33:37 +0900

    Modified response format

--- src/main/java/shop/samgak/mini_board/post/controllers/PostController.java
+++ src/main/java/shop/samgak/mini_board/post/controllers/PostController.java
@@ -1,7 +1,6 @@
 package shop.samgak.mini_board.post.controllers;
 
 import java.net.URI;
-import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,12 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 import com.fasterxml.jackson.core.JsonProcessingException;
 
 import jakarta.servlet.http.HttpSession;
-import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import shop.samgak.mini_board.post.dto.PostDTO;
+import shop.samgak.mini_board.exceptions.MissingParameterException;
 import shop.samgak.mini_board.post.services.PostService;
 import shop.samgak.mini_board.user.entities.User;
+import shop.samgak.mini_board.utility.ApiDataResponse;
 import shop.samgak.mini_board.utility.ApiResponse;
 import shop.samgak.mini_board.utility.UserSessionHelper;
 
@@ -32,31 +31,29 @@ public class PostController {
     private final UserSessionHelper userSessionHelper;
 
     @GetMapping
-    public List<PostDTO> getAllPost() {
-        return postService.getAll();
+    public ResponseEntity<ApiResponse> getAllPost() {
+        return ResponseEntity.ok(new ApiDataResponse("success", postService.getAll(), true));
     }
 
     @GetMapping("/{id}")
-    public PostDTO getPost(@PathVariable Long id) {
-        return postService.getPostById(id);
+    public ResponseEntity<ApiResponse> getPost(@PathVariable Long id) {
+        return ResponseEntity.ok(new ApiDataResponse("success", postService.getPostById(id), true));
     }
 
     @PostMapping
-    public ResponseEntity<ApiResponse> createPost(@RequestParam PostCreateRequest postCreateRequest,
+    public ResponseEntity<ApiResponse> createPost(@RequestParam String title, @RequestParam String content,
             HttpSession session) {
+        if (title == null || title.isEmpty())
+            throw new MissingParameterException("username");
+        if (content == null || content.isEmpty())
+            throw new MissingParameterException("content");
         try {
             User user = userSessionHelper.getCurrentUserFromSession(session).orElseThrow();
-            Long createdId = postService.create(postCreateRequest.getTitle(), postCreateRequest.getContent(), user);
+            Long createdId = postService.create(title, content, user);
             URI location = URI.create(String.format("/api/post/%d", createdId));
             return ResponseEntity.created(location).body(new ApiResponse("success", true));
         } catch (RuntimeException | JsonProcessingException e) {
             return ResponseEntity.internalServerError().body(new ApiResponse(e.getMessage(), false));
         }
-    }
-
-    @Data
-    public class PostCreateRequest {
-        private String title;
-        private String content;
     }
 }
