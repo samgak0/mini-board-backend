b9705f31d2e7119f6bcf0b3ba3506cb0d5487c8d
samgak <183370871+samgak0@users.noreply.github.com>
2024-10-22 19:19:03 +0900

    Fixed instant serialization

--- src/main/java/shop/samgak/mini_board/config/JacksonConfig.java
+++ src/main/java/shop/samgak/mini_board/config/JacksonConfig.java
@@ -1,9 +1,19 @@
 package shop.samgak.mini_board.config;
 
+import java.io.IOException;
+import java.time.Instant;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
@@ -15,6 +25,26 @@ public class JacksonConfig {
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.registerModule(new JavaTimeModule());
         objectMapper.registerModule(new Jdk8Module());
+
+        SimpleModule module = new SimpleModule();
+        module.addSerializer(Instant.class, new UnixTimestampSerializer());
+        module.addDeserializer(Instant.class, new UnixTimestampDeserializer());
+        objectMapper.registerModule(module);
+
         return objectMapper;
+    }
+
+    public class UnixTimestampSerializer extends JsonSerializer<Instant> {
+        @Override
+        public void serialize(Instant instant, JsonGenerator gen, SerializerProvider serializers) throws IOException {
+            gen.writeNumber(instant.getEpochSecond());
+        }
+    }
+
+    public class UnixTimestampDeserializer extends JsonDeserializer<Instant> {
+        @Override
+        public Instant deserialize(JsonParser p, DeserializationContext ctxt) throws IOException {
+            return Instant.ofEpochSecond(p.getLongValue());
+        }
     }
 }
