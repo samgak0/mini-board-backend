b682dff1aff2bdb5a8d76e4780967876ffd21869
samgak <183370871+samgak0@users.noreply.github.com>
2024-10-22 12:14:45 +0900

    Modifid enable default typing in ObjectMapper

--- src/main/java/shop/samgak/mini_board/config/RedisConfig.java
+++ src/main/java/shop/samgak/mini_board/config/RedisConfig.java
@@ -12,32 +12,34 @@ import org.springframework.lang.NonNull;
 import org.springframework.security.jackson2.SecurityJackson2Modules;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
 
 @Configuration
 @EnableRedisHttpSession
 public class RedisConfig implements BeanClassLoaderAware {
 
 	private ClassLoader loader;
-    
+
 	@Bean
 	public RedisSerializer<Object> springSessionDefaultRedisSerializer() {
 		return new GenericJackson2JsonRedisSerializer(objectMapper());
 	}
 
-    @Bean
-    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
-        RedisTemplate<String, Object> template = new RedisTemplate<>();
-        template.setConnectionFactory(connectionFactory);
+	@Bean
+	public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+		RedisTemplate<String, Object> template = new RedisTemplate<>();
+		template.setConnectionFactory(connectionFactory);
 
-        template.setKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(new GenericJackson2JsonRedisSerializer(objectMapper()));
-        template.setHashKeySerializer(new StringRedisSerializer());
-        template.setHashValueSerializer(new GenericJackson2JsonRedisSerializer(objectMapper()));
+		template.setKeySerializer(new StringRedisSerializer());
+		template.setValueSerializer(new GenericJackson2JsonRedisSerializer(objectMapper()));
+		template.setHashKeySerializer(new StringRedisSerializer());
+		template.setHashValueSerializer(new GenericJackson2JsonRedisSerializer(objectMapper()));
 
-        template.afterPropertiesSet();
-        return template;
-    }
+		template.afterPropertiesSet();
+		return template;
+	}
 
 	@Override
 	public void setBeanClassLoader(@NonNull ClassLoader classLoader) {
@@ -45,8 +47,11 @@ public class RedisConfig implements BeanClassLoaderAware {
 	}
 
 	private ObjectMapper objectMapper() {
-		ObjectMapper mapper = new ObjectMapper();
-		mapper.registerModules(SecurityJackson2Modules.getModules(this.loader));
-		return mapper;
+		ObjectMapper objectMapper = new ObjectMapper();
+		objectMapper.activateDefaultTyping(LaissezFaireSubTypeValidator.instance, ObjectMapper.DefaultTyping.NON_FINAL,
+				JsonTypeInfo.As.PROPERTY);
+		objectMapper.registerModules(SecurityJackson2Modules.getModules(this.loader));
+
+		return objectMapper;
 	}
 }
--- src/main/java/shop/samgak/mini_board/user/dto/UserDTO.java
+++ src/main/java/shop/samgak/mini_board/user/dto/UserDTO.java
@@ -1,11 +1,8 @@
 package shop.samgak.mini_board.user.dto;
 
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
-
 import lombok.Data;
 
 @Data
-@JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, property = "@class")
 public class UserDTO {
     private Long id;
     private String username;
