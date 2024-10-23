76ae7911421fd3af349f116ea3b2c20fabb1a3fc
samgak <183370871+samgak0@users.noreply.github.com>
2024-10-23 19:18:42 +0900

    Added Spring Validation

--- src/main/java/shop/samgak/mini_board/security/AuthController.java
+++ src/main/java/shop/samgak/mini_board/security/AuthController.java
@@ -1,11 +1,9 @@
 package shop.samgak.mini_board.security;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,8 +14,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import shop.samgak.mini_board.exceptions.UserNotLoginException;
 import shop.samgak.mini_board.utility.ApiResponse;
 import shop.samgak.mini_board.utility.AuthUtils;
 
@@ -30,18 +31,18 @@ public class AuthController {
     private final AuthenticationManager authenticationManager;
 
     @PostMapping("login")
-    public ResponseEntity<ApiResponse> login(@RequestBody LoginRequest loginRequest, HttpSession session) {
-        try {
-            UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
-                    loginRequest.username, loginRequest.password);
-            Authentication authentication = authenticationManager.authenticate(authenticationToken);
-            SecurityContextHolder.getContext().setAuthentication(authentication);
-            AuthUtils.saveSessionSecurityContext(SecurityContextHolder.getContext(), session);
-            return ResponseEntity.ok().body(new ApiResponse("Login successful", true));
-        } catch (AuthenticationException e) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
-                    .body(new ApiResponse("Invalid credentials : " + e.getMessage(), false));
-        }
+    public ResponseEntity<ApiResponse> login(@Valid @RequestBody LoginRequest loginRequest,
+            HttpSession session) {
+
+        log.info(loginRequest.username);
+        log.info(loginRequest.password);
+
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                loginRequest.username, loginRequest.password);
+        Authentication authentication = authenticationManager.authenticate(authenticationToken);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        AuthUtils.saveSessionSecurityContext(SecurityContextHolder.getContext(), session);
+        return ResponseEntity.ok().body(new ApiResponse("Login successful", true));
     }
 
     @GetMapping("logout")
@@ -55,17 +56,18 @@ public class AuthController {
             if (session.getAttribute(HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY) != null) {
                 session.invalidate();
             } else {
-                return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
-                        .body(new ApiResponse("User not logged in", false));
+                throw new UserNotLoginException();
             }
         } else {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new ApiResponse("User not logged in", false));
+            throw new UserNotLoginException();
         }
         SecurityContextHolder.clearContext();
 
         return ResponseEntity.ok().body(new ApiResponse("Logout successful", true));
     }
 
-    public record LoginRequest(String username, String password) {
+    public record LoginRequest(
+            @NotNull(message = "Missing required parameter: username") String username,
+            @NotNull(message = "Missing required parameter: password") String password) {
     }
 }
