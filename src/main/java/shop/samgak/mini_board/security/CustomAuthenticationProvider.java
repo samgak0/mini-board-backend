1ba0833798504d6d00553868974d34647cfa0a33
samgak <183370871+samgak0@users.noreply.github.com>
2024-10-31 15:49:48 +0900

    로그인 처리 주석 추가

--- src/main/java/shop/samgak/mini_board/security/CustomAuthenticationProvider.java
+++ src/main/java/shop/samgak/mini_board/security/CustomAuthenticationProvider.java
@@ -7,44 +7,86 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import lombok.RequiredArgsConstructor;
-import shop.samgak.mini_board.exceptions.UserNotExistFoundException;
+import lombok.extern.slf4j.Slf4j;
+import shop.samgak.mini_board.exceptions.UserNotFoundException;
 import shop.samgak.mini_board.exceptions.WrongPasswordException;
 import shop.samgak.mini_board.user.dto.UserDTO;
 import shop.samgak.mini_board.user.entities.User;
 import shop.samgak.mini_board.user.mapper.UserMapper;
 import shop.samgak.mini_board.user.repositories.UserRepository;
 
+/**
+ * CustomAuthenticationProvider는 Spring Security의 AuthenticationProvider를 구현하여
+ * 사용자 인증 로직을 커스터마이징
+ */
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class CustomAuthenticationProvider implements AuthenticationProvider {
 
+    // 비밀번호 인코더, 외부에서 주입받아 사용
     private PasswordEncoder passwordEncoder;
 
+    // 사용자 정보를 조회하기 위한 UserRepository
     private final UserRepository userRepository;
+    // User 엔티티를 DTO로 변환하기 위한 매퍼
     private final UserMapper userMapper;
 
+    /**
+     * PasswordEncoder 설정 메서드
+     * 
+     * @param passwordEncoder 비밀번호를 암호화하고 검증하는 인코더
+     */
     public void setPasswordEncoder(PasswordEncoder passwordEncoder) {
         this.passwordEncoder = passwordEncoder;
     }
 
+    /**
+     * 실제 인증을 처리하는 메서드
+     * 
+     * @param authentication 인증 요청 객체로, 사용자명과 비밀번호를 포함함
+     * @return 인증이 성공한 경우 Authentication 객체 반환
+     * @throws UserNotFoundException  사용자 이름이 존재하지 않을 때 발생
+     * @throws WrongPasswordException 비밀번호가 일치하지 않을 때 발생
+     */
     @Override
     public Authentication authenticate(Authentication authentication) {
+        // 인증 요청에서 사용자명과 비밀번호 추출함
         String username = authentication.getName();
         String password = (String) authentication.getCredentials();
 
+        // 사용자명으로 UserRepository에서 사용자 정보 조회함
+        log.info("사용자 [{}] 인증 시도 중", username);
         User user = userRepository.findByUsername(username)
-                .orElseThrow(() -> new UserNotExistFoundException(username));
+                .orElseThrow(() -> {
+                    log.warn("사용자 [{}]를 찾을 수 없음", username);
+                    return new UserNotFoundException(username);
+                }); // 사용자가 존재하지 않으면 예외 발생
+
+        // User 엔티티를 UserDTO로 변환함
         UserDTO userDTO = userMapper.userToUserDTO(user);
 
+        // UserDTO와 비밀번호로 MyUserDetails 객체 생성
         MyUserDetails myUserDetails = new MyUserDetails(userDTO, user.getPassword());
+        // 입력된 비밀번호와 저장된 비밀번호 비교함
         if (!passwordEncoder.matches(password, myUserDetails.getPassword())) {
-            throw new WrongPasswordException(username);
+            log.warn("사용자 [{}]의 비밀번호가 일치하지 않음", username);
+            throw new WrongPasswordException(username); // 비밀번호가 일치하지 않으면 예외 발생
         }
+        // 인증 후 비밀번호는 null로 설정하여 보안 강화함
         myUserDetails.setPassword(null);
 
+        log.info("사용자 [{}] 인증 성공", username);
+        // 인증이 성공하면 UsernamePasswordAuthenticationToken 반환함
         return new UsernamePasswordAuthenticationToken(myUserDetails, null, myUserDetails.getAuthorities());
     }
 
+    /**
+     * 이 AuthenticationProvider가 특정 인증 유형을 지원하는지 여부 반환
+     * 
+     * @param authentication 인증 객체의 클래스
+     * @return 지원 여부 (UsernamePasswordAuthenticationToken이면 true 반환)
+     */
     @Override
     public boolean supports(Class<?> authentication) {
         return UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication);
--- src/main/java/shop/samgak/mini_board/security/MyUserDetails.java
+++ src/main/java/shop/samgak/mini_board/security/MyUserDetails.java
@@ -15,34 +15,67 @@ import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import lombok.Data;
 import shop.samgak.mini_board.user.dto.UserDTO;
 
+/**
+ * MyUserDetails 클래스는 Spring Security의 UserDetails 인터페이스를 구현하여 사용자 정보를 관리하는 역할을
+ * 함
+ */
 @Data
 @JsonTypeInfo(use = JsonTypeInfo.Id.CLASS)
 public class MyUserDetails implements UserDetails {
+    // 사용자 정보를 저장하는 UserDTO 객체
     private UserDTO userDTO;
+    // 사용자 비밀번호
     private String password;
+    // 사용자 권한 목록
     private List<GrantedAuthority> authorities;
+    // 계정 만료 여부 (기본값: 만료되지 않음)
     private boolean isAccountNonExpired = true;
+    // 계정 잠금 여부 (기본값: 잠기지 않음)
     private boolean isAccountNonLocked = true;
+    // 자격 증명 만료 여부 (기본값: 만료되지 않음)
     private boolean isCredentialsNonExpired = true;
+    // 계정 활성화 여부 (기본값: 활성화됨)
     private boolean isEnabled = true;
 
+    /**
+     * JSON으로부터 객체를 생성하기 위한 생성자
+     * 
+     * @param user     UserDTO 객체
+     * @param password 사용자 비밀번호
+     */
     @JsonCreator
     public MyUserDetails(@JsonProperty("user") UserDTO user, @JsonProperty("password") String password) {
         this.userDTO = user;
         this.password = password;
         this.authorities = new ArrayList<>();
+        // 기본 권한으로 ROLE_USER 추가
         this.authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
     }
 
+    /**
+     * UserDTO 객체 반환
+     * 
+     * @return UserDTO 사용자 정보 객체
+     */
     public UserDTO getUserDTO() {
         return userDTO;
     }
 
+    /**
+     * UserDTO 객체 설정
+     * 
+     * @param user 사용자 정보 객체
+     */
     @JsonProperty("user")
     public void setUserDTO(UserDTO user) {
         this.userDTO = user;
     }
 
+    /**
+     * 사용자 이름 설정
+     * 
+     * @param username 사용자 이름
+     */
     public void setUsername(String username) {
         if (this.userDTO == null) {
             this.userDTO = new UserDTO();
@@ -50,11 +83,21 @@ public class MyUserDetails implements UserDetails {
         this.userDTO.setUsername(username);
     }
 
+    /**
+     * 사용자 이름 반환
+     * 
+     * @return 사용자 이름
+     */
     @Override
     public String getUsername() {
         return userDTO.getUsername();
     }
 
+    /**
+     * 사용자 ID 설정
+     * 
+     * @param id 사용자 ID
+     */
     public void setId(Long id) {
         if (this.userDTO == null) {
             this.userDTO = new UserDTO();
@@ -62,56 +105,116 @@ public class MyUserDetails implements UserDetails {
         userDTO.setId(id);
     }
 
+    /**
+     * 사용자 비밀번호 반환
+     * 
+     * @return 사용자 비밀번호
+     */
     @Override
     public String getPassword() {
         return this.password;
     }
 
+    /**
+     * 사용자 비밀번호 설정
+     * 
+     * @param password 사용자 비밀번호
+     */
     public void setPassword(String password) {
         this.password = password;
     }
 
+    /**
+     * 사용자 권한 목록 반환
+     * 
+     * @return 사용자 권한 목록
+     */
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return authorities;
     }
 
+    /**
+     * 사용자 권한 목록 설정
+     * 
+     * @param authorities 사용자 권한 목록
+     */
     public void setAuthorities(List<GrantedAuthority> authorities) {
         this.authorities = authorities;
     }
 
+    /**
+     * 계정 만료 여부 반환
+     * 
+     * @return 계정이 만료되지 않았으면 true 반환
+     */
     @Override
     public boolean isAccountNonExpired() {
         return this.isAccountNonExpired;
     }
 
+    /**
+     * 계정 만료 여부 설정
+     * 
+     * @param isAccountNonExpired 계정 만료 여부
+     */
     public void setAccountNonExpired(boolean isAccountNonExpired) {
         this.isAccountNonExpired = isAccountNonExpired;
     }
 
+    /**
+     * 계정 잠금 여부 반환
+     * 
+     * @return 계정이 잠기지 않았으면 true 반환
+     */
     @Override
     public boolean isAccountNonLocked() {
         return this.isAccountNonLocked;
     }
 
+    /**
+     * 계정 잠금 여부 설정
+     * 
+     * @param isAccountNonLocked 계정 잠금 여부
+     */
     public void setAccountNonLocked(boolean isAccountNonLocked) {
         this.isAccountNonLocked = isAccountNonLocked;
     }
 
+    /**
+     * 자격 증명 만료 여부 반환
+     * 
+     * @return 자격 증명이 만료되지 않았으면 true 반환
+     */
     @Override
     public boolean isCredentialsNonExpired() {
         return this.isCredentialsNonExpired;
     }
 
+    /**
+     * 자격 증명 만료 여부 설정
+     * 
+     * @param isCredentialsNonExpired 자격 증명 만료 여부
+     */
     public void setCredentialsNonExpired(boolean isCredentialsNonExpired) {
         this.isCredentialsNonExpired = isCredentialsNonExpired;
     }
 
+    /**
+     * 계정 활성화 여부 반환
+     * 
+     * @return 계정이 활성화되었으면 true 반환
+     */
     @Override
     public boolean isEnabled() {
         return this.isEnabled;
     }
 
+    /**
+     * 계정 활성화 여부 설정
+     * 
+     * @param isEnabled 계정 활성화 여부
+     */
     public void setEnabled(boolean isEnabled) {
         this.isEnabled = isEnabled;
     }
